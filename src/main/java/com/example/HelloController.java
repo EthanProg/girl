11731fae6be25959d98d868a1c199b7bae74873a
ethan <programcccethan@gmail.com>
2017-01-20 10:24:06 +0800

    push

--- src/main/java/com/example/HelloController.java
+++ src/main/java/com/example/HelloController.java
@@ -1,5 +1,6 @@
 package com.example;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -20,9 +21,12 @@ public class HelloController {
     @Value("${content}")
     private String content;
 
+    @Autowired
+    private GirlProperties girlProperties;
+
 
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String say(){
-        return content;
+        return girlProperties.getCupSize();
     }
 }
