fcca6071b1454aada9d6d87da5999e9bd635fcdd
ethan <programcccethan@gmail.com>
2018-05-31 15:38:46 +0800

    push

--- src/main/java/com/example/HelloController.java
+++ src/main/java/com/example/HelloController.java
@@ -1,7 +1,9 @@
 package com.example;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +14,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+    private final Logger logger = Logger.getLogger(getClass());
+
 //    @Value("${cupSize}")
 //    private String cupSize;
 //
@@ -21,11 +25,21 @@ public class HelloController {
 //    @Value("${content}")
 //    private String content;
 
-    @Autowired
-    private GirlProperties girlProperties;
+//    @Autowired
+//    private GirlProperties girlProperties;
+//
+//    @RequestMapping(value = {"/hello", "hi"}, method = RequestMethod.GET)
+//    public String say() {
+//        return girlProperties.getCupSize();
+//    }
 
-    @RequestMapping(value = {"/hello", "hi"}, method = RequestMethod.GET)
-    public String say() {
-        return girlProperties.getCupSize();
+    @Autowired
+    private DiscoveryClient client;
+
+    @RequestMapping(value = "/hi", method = RequestMethod.GET)
+    public String index(){
+        ServiceInstance instance = client.getLocalServiceInstance();
+        logger.info("/hi, host: " + instance.getHost() + ", service_id: " + instance.getServiceId());
+        return "Hello World!";
     }
 }
